5f325b6333cda8224104ec10de33dd7db2c38fce
Patrick Miziewicz <patrick.miziewicz@gmail.com>
2018-08-14 19:17:10 +1000

    Added a very basic account confirmation page that shows identity server response. WIP
    Signed-off-by: Patrick Miziewicz <patrick.miziewicz@gmail.com>

--- src/components/pages/AccountConfirmationPage.vue
+++ src/components/pages/AccountConfirmationPage.vue
@@ -1,14 +1,40 @@
 <template>
     <div>
       ACCOUNT HAS BEEN CONFIRMED
+      <br><br>
+      {{identityServerResponse}}
     </div>
 </template>
 
 <script>
+    import {confirm} from '../../lib/auth';
     export default {
         name: "AccountConfirmationPage",
         mounted() {
-          console.log('extracting url params: ', this.$route.query.page);
+          console.log('extracting url params: ', this.$route);
+          this.confirmAccount()
+            .then((res) => {
+              console.log("Successfully confirmed the account: ", res);
+              this.identityServerResponse = res;
+            })
+            .catch((err) => {
+              console.log("An error occurred when confirming the token: ", err);
+              this.identityServerResponse = err;
+            })
+        },
+        data() {
+          return {
+            identityServerResponse: "",
+          }
+        },
+        methods: {
+          extractConfirmationToken(fullPathRoute) {
+            return fullPathRoute.replace("/confirmation_token=", "")
+          },
+          confirmAccount() {
+            console.log("Confirming account with token: ", this.extractConfirmationToken(this.$route.fullPath));
+            return confirm(this.extractConfirmationToken(this.$route.fullPath));
+          }
         }
     }
 </script>
