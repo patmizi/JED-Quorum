fd3235878918224fd5fe2ab32006616ff53d4602
Patrick Miziewicz <lio.patrickmiz@gmail.com>
2018-08-15 16:25:46 +1000

    Removed manual account confirmation trigger because accounts are automatically confirmed anyways when they click the email link.
    Signed-off-by: Patrick Miziewicz <patrick.miziewicz@gmail.com>

--- src/components/pages/AccountConfirmationPage.vue
+++ src/components/pages/AccountConfirmationPage.vue
@@ -1,41 +1,18 @@
 <template>
     <div>
-      ACCOUNT HAS BEEN CONFIRMED
-      <br><br>
-      {{identityServerResponse}}
+      ACCOUNT HAS BEEN CONFIRMED. YOU WILL BE REDIRECTED SHORTLY...
     </div>
 </template>
 
 <script>
-    import {confirm} from '../../lib/auth';
     export default {
         name: "AccountConfirmationPage",
         mounted() {
           console.log('extracting url params: ', this.$route);
-          this.confirmAccount()
-            .then((res) => {
-              console.log("Successfully confirmed the account: ", res);
-              this.identityServerResponse = res;
-            })
-            .catch((err) => {
-              console.log("An error occurred when confirming the token: ", err);
-              this.identityServerResponse = err;
-            })
+          setTimeout(() => {
+            this.$router.push('login');
+          }, 2000);
         },
-        data() {
-          return {
-            identityServerResponse: "",
-          }
-        },
-        methods: {
-          extractConfirmationToken(fullPathRoute) {
-            return fullPathRoute.replace("/confirmation_token=", "")
-          },
-          confirmAccount() {
-            console.log("Confirming account with token: ", this.extractConfirmationToken(this.$route.fullPath));
-            return confirm(this.extractConfirmationToken(this.$route.fullPath));
-          }
-        }
     }
 </script>
 
